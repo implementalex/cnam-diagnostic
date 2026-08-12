b225c3df05bf3eeae895a183df4c135d819733e4
Alexey <implementalex@gmail.com>
2026-08-12 16:16:44 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,15 @@
-1
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "ru.cnamdiagnostic"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "ru.cnamdiagnostic"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
